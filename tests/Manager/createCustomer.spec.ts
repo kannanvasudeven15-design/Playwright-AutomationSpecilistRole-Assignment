85ff1323d1ae6017dc08631e0e4d578cc11242a7
Kannan Vasudeven <kannanvasudeven15@gmail.com>
2025-12-07 18:24:58 +0000

    Updates

--- tests/Manager/createCustomer.spec.ts
+++ tests/Manager/createCustomer.spec.ts
@@ -94,7 +94,7 @@ test('Test 2: Duplicate Customer scenario - should show duplicate message when c
   }
 });
 
-test('Test 3: Field validation scenario - should require firstname lastname postcode for adding customer', async ({ page, managerPage }) => {
+test('Test 3: Field validation scenario - should require firstname lastname postcode for adding customer',{ tag: ['@PlaywrightWithGitHubActions'] }, async ({ page, managerPage }) => {
   const csvPath = path.resolve(__dirname, '../../test-data/AddCustomerFieldValidation.csv');
   const customerSets = readCsvSets(csvPath);
   for (const customerData of customerSets) {
